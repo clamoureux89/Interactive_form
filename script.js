e7f37335b9e59e7077c144c6110da12854e7f17e
clamoureux89 <51682100+clamoureux89@users.noreply.github.com>
2019-06-27 20:06:02 -0400

    addition of validators for cc info

--- script.js
+++ script.js
@@ -6,35 +6,42 @@ let sizeField = document.getElementById("size")
 let designField = document.getElementById("design")
 let colorField = document.getElementById("color")
 let activitiesField = document.querySelector(".activities")
+let zip = document.getElementById("zip")
+let cvv = document.getElementById("cvv")
+let creditcard = document.getElementById("cc-num")
+
+
 
 ///Upon loading focus on the first input field so user can start typing
 nameField.focus()
 
-///Create a general validator function which needs only a target and a base regex
-function createListener(validator){
-    return e =>{
-        const text= e.target.textContent;
-        let valid = validator(text);
-        if (valid = false) {
-            body.style.backgroundColor = "rgb(189, 86, 86)";
-          }
-        }
-    }
 
 ///Validator for the name input field
-function isValidUsername(username){
-    return /^[a-z]+$/.test(username);
-}
 
-nameField.addEventListener("focusout", createListener(isValidUsername))
+nameField.addEventListener("focusout",()=>{
+    console.log(nameField.value)
+    valid = /^[a-zA-Z\s]+$/.test(nameField.value)
+    if (valid) {
+        nameField.style.backgroundColor = "lightgreen";
+      }else{
+        nameField.style.backgroundColor = "red";
+      }
+    console.log(nameField.value +"checked")
+})
 
 
 ///Validator for the email field
-function isValidEmail(email){
-    return /^[^@]+@[^@.]+\.[a-z]{3}$/i.test(email);
-}
 
-mailField.addEventListener("focusout", createListener(isValidEmail))
+mailField.addEventListener("focusout", ()=>{
+    console.log(mailField.value)
+    valid= /^[^@]+@[^@.]+\.[a-z]{3}$/i.test(mailField.value);
+    if (valid) {
+    mailField.style.backgroundColor = "lightgreen";
+  }else{
+      mailField.style.backgroundColor = "red"
+  }
+    console.log(mailField.value +"checked")
+})
 
 
 ///Add feature to other in job roles so a new input field is displayed when other is selected
@@ -55,37 +62,39 @@ let tomato =gold.nextSibling
 let steelblue =tomato.nextSibling
 let dimgrey = steelblue.nextSibling
 
-const HideColors = () => {
-    colorField.hidden = true
-    cornflower.hidden=true
-    darkslategrey.hidden=true
-    gold.hidden = true
-    tomato.hidden = true
-    steelblue.hidden = true
-    dimgrey.hidden = true
-}
 
-HideColors()
+colorField.hidden = true
+cornflower.hidden=true
+darkslategrey.hidden=true
+gold.hidden = true
+tomato.hidden = true
+steelblue.hidden = true
+dimgrey.hidden = true
 
 
 ///Change available colors based on design chosen, if none revert back to none shown
-designField.addEventListener("change", ()=>{
-    if(designField.textContent = 'js puns'){
+designField.addEventListener("select", ()=>{
+    if(designField.value = 'js puns'){
         colorField.hidden = false
         cornflower.hidden = false
         darkslategrey.hidden = false
         gold.hidden = false
-    }if(designField.textContent = 'heart js'){
+        tomato.hidden = true
+        steelblue.hidden = true
+        dimgrey.hidden = true
+    }if(designField.value = 'heart js'){
         colorField.hidden = false
         tomato.hidden = false
         steelblue.hidden = false
         dimgrey.hidden= false
-    }else{
-        HideColors()
+        colorField.hidden = true
+        cornflower.hidden=true
+        darkslategrey.hidden=true
     }
 })
 
 let showcost = document.createElement('p')
+
 
 cost = 0
 
@@ -93,36 +102,72 @@ activitiesField.addEventListener('change',(e)=>{
     let selectedcost = e.target.parentElement.textContent
     if (e.target.checked){
         index = selectedcost.indexOf(('$')+1)
-        cost += parseInt(selectedcost.slice(index))
+        cost += parseInt(selectedcost.slice(index,))
     }
     else{
         index = selectedcost.indexOf(('$')+1)
-        cost += parseInt(selectedcost.slice(index))
+
+        cost += parseInt(selectedcost.slice(index,))
     }
 })
-showcost.innerHTML = "Total Cost : $" + cost
+showcost.innerHTML = "<p>Total Cost : $" + cost + "</p>"
 activitiesField.appendChild(showcost)
 
 
 
 const payfield = document.getElementById("payment")
-const creditcard = document.getElementById("credit-card")
-const paypal = creditcard.nextSibling
-const bitcoin = paypal.nextSibling
+const selectcredit = document.getElementById("credit-card")
+const selectpaypal = selectcredit.nextSibling
+const selectbitcoin = selectpaypal.nextSibling
 
-paypal.style = "hidden"
-bitcoin.style = "hidden"
+selectpaypal.style = "hidden"
+selectbitcoin.style = "hidden"
 payfield.addEventListener("change", (e) => {
-    creditcard.style = "hidden"
-    paypal.style = "hidden"
-    bitcoin.style = "hidden"
-    if(e.value === "Credit Card"){
+    selectcredit.style = "hidden"
+    selectpaypal.style = "hidden"
+    selectbitcoin.style = "hidden"
+    if(payfield.value === "Credit Card"){
         creditcard.style ==="block"
     }
-    if(e.value === "Paypal"){
+    if(payfield.value === "Paypal"){
         paypal.style ==="block"
     }
-    if(e.value === "Bitcoin"){
+    if(payfield.value === "Bitcoin"){
         bitcoin.style ==="block"
     }
 }) 
+
+
+creditcard.addEventListener("focusout", ()=>{
+    console.log(creditcard.value)
+    valid= /^[0-9]{16}$/i.test(creditcard.value);
+    if (valid) {
+    creditcard.style.backgroundColor = "lightgreen";
+  }else{
+      creditcard.style.backgroundColor = "red"
+  }
+    console.log(creditcard.value +"checked")
+})
+
+cvv.addEventListener("focusout", ()=>{
+    console.log(cvv.value)
+    valid= /^[0-9]{3}$/i.test(cvv.value);
+    if (valid) {
+    cvv.style.backgroundColor = "lightgreen";
+  }else{
+      cvv.style.backgroundColor = "red"
+  }
+    console.log(cvv.value +"checked")
+})
+
+
+zip.addEventListener("focusout", ()=>{
+    console.log(zip.value)
+    valid= /^[0-9]{6}$/i.test(zip.value);
+    if (valid) {
+    zip.style.backgroundColor = "lightgreen";
+  }else{
+      zip.style.backgroundColor = "red"
+  }
+    console.log(zip.value +"checked")
+})

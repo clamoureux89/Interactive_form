174f1af400a8dab5df5144a5db4ce943f29e5acb
clamoureux89 <51682100+clamoureux89@users.noreply.github.com>
2019-12-30 18:29:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -128,7 +128,7 @@ activitiesField.addEventListener('change',(e)=>{
         for(i=0;i<morning.length;i++){
             if(morning[i] != e.target.name){
                 document.getElementsByName(morning[i]).disabled = true
-                console.log(document.getElementsByName(morning[i]))
+                console.log(document.getElementsByName(morning[i]))}
         }
         
     }
@@ -144,7 +144,7 @@ activitiesField.addEventListener('change',(e)=>{
         for(i=0;i<afternoon.length;i++){
             if(afternoon[i] != e.target.name){
                 document.getElementsByName(afternoon[i]).disabled = true
-                console.logdocument.getElementsByName(afternoon[i])
+                console.log(document.getElementsByName(afternoon[i]))
             }
         }
     }
